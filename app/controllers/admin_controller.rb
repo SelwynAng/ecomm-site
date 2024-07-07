dd982ea3dcec211ca9574f4d53c8db973772522c
Selwyn Ang <selwynang.01@gmail.com>
2024-07-07 20:22:47 +0800

    Edit admin controller and view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,16 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-
+    @current_admin_email = current_admin.email
+    @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+    @quick_stats = {
+      sales: Order.where(created_at: Time.now.midnight..Time.now).count,
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(),
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(),
+      per_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight..Time.now}).average(:quantity)
+    }
+    @orders_by_day = Order.where('created_at > ?', Time.now - 7.days).order(:created_at)
+    @orders_by_day = @orders_by_day.group_by { |order| order.created_at.to_date }
+    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), orders.sum(&:total)] }
   end
 end
